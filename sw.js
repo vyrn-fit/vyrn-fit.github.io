1755050b0af17b8543b598df8d66d133d1819000
vyrn-fit <vyrn.support@proton.me>
2026-08-28 16:37:50 +0800

    Bump SW cache to v34 for reps + coach update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vyrn-v33';
+const CACHE = 'vyrn-v34';
 const ASSETS = ['/', '/index.html', '/app.js', '/site.js', '/manifest.json', '/assets/logo.png'];
 
 self.addEventListener('install', (e) => {
