62e9f13c7ca248fb09c62ceb02184487cdef3ea9
vyrn-fit <vyrn.support@proton.me>
2026-08-29 11:22:52 +0800

    Bump SW to v37 for accurate equipment-free guides

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vyrn-v36';
+const CACHE = 'vyrn-v37';
 const ASSETS = ['/', '/index.html', '/app.js', '/site.js', '/manifest.json', '/assets/logo.png'];
 
 self.addEventListener('install', (e) => {
