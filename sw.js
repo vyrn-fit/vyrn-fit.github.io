c3f38843c74e678ddeb21bd0342aee60993bee9e
vyrn-fit <vyrn.support@proton.me>
2026-08-29 16:27:21 +0800

    Bump SW v39 for auth UX

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vyrn-v38';
+const CACHE = 'vyrn-v39';
 const ASSETS = ['/', '/index.html', '/app.js', '/site.js', '/manifest.json', '/assets/logo.png'];
 
 self.addEventListener('install', (e) => {
