0a2014226e7630d9f291bcb969702b44cd65df71
vyrn-fit <vyrn.support@proton.me>
2026-08-29 16:35:08 +0800

    Bump SW v40 for visible exercise frames fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vyrn-v39';
+const CACHE = 'vyrn-v40';
 const ASSETS = ['/', '/index.html', '/app.js', '/site.js', '/manifest.json', '/assets/logo.png'];
 
 self.addEventListener('install', (e) => {
