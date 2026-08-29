17e3c2b9bb919690726ec0552e0fab55d4798f96
vyrn-fit <vyrn.support@proton.me>
2026-08-29 11:15:44 +0800

    Bump SW to v36 for exercise guide mapping fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vyrn-v35';
+const CACHE = 'vyrn-v36';
 const ASSETS = ['/', '/index.html', '/app.js', '/site.js', '/manifest.json', '/assets/logo.png'];
 
 self.addEventListener('install', (e) => {
