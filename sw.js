986b16449e4329ffbedec184c61237c936644d51
vyrn-fit <vyrn.support@proton.me>
2026-08-30 10:05:22 +0800

    Bump SW v42 no stick figures

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vyrn-v41';
+const CACHE = 'vyrn-v42';
 const ASSETS = ['/', '/index.html', '/app.js', '/site.js', '/manifest.json', '/assets/logo.png'];
 
 self.addEventListener('install', (e) => {
